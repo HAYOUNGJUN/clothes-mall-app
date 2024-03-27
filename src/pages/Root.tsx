67914722311c041a17e777502dc62b9bfabb16a6
Youngjun Ha <dudwnsk011@gmail.com>
2024-03-27 19:15:28 +0900

    Add Header

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,3 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Navigation/Header';
+
 export default function Root() {
-  return <div>Root</div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
