5e89cdc5682c09826688a69bf93f9bdf42a69395
Youngjun Ha <dudwnsk011@gmail.com>
2024-03-30 18:58:11 +0900

    modify css

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -41,38 +41,65 @@ export default function Product() {
   }
 
   if (productData) {
+    // content = (
+    //   <>
+    //     <div className='w-96'>
+    //       <img src={productData.img} alt={productData.title} />
+    //     </div>
+    //     <div className='ml-8'>
+    //       <div className='text-xl my-2 font-bold'>{productData.title}</div>
+    //       <div className='text-lg my-2'>₩{productData.price}</div>
+    //       <div className='text-sm my-2 font-thin'>
+    //         {productData.choice ? 'Conscious choice' : ''}
+    //       </div>
+    //       <div className='my-2'>
+    //         <div>사이즈</div>
+    //
+    //         <ul className='flex'>
+    //           {productData.size.map((sizeOption, key) => (
+    //             <li key={key}>
+    //               <button className='border-2 border-stone-800 rounded-md px-12 py-4 mx-2 my-2 cursor-pointer focus:border-red-500'>
+    //                 {sizeOption}
+    //               </button>
+    //             </li>
+    //           ))}
+    //         </ul>
+    //       </div>
+    //       <div className='my-2'>
+    //         <button className='bg-stone-800 text-stone-100 rounded p-2 w-96 hover:bg-stone-600'>
+    //           추가
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </>
+    // );
     content = (
       <>
-        <div className='w-96'>
+        <div className='my-2 justify-self-center md:w-96 md:justify-self-end'>
           <img src={productData.img} alt={productData.title} />
         </div>
-        <div className='ml-8'>
-          <div className='text-xl my-2 font-bold'>{productData.title}</div>
-          <div className='text-lg my-2'>₩{productData.price}</div>
-          <div className='text-sm my-2 font-thin'>
+        <div className='md:ml-8 md:justify-self-start'>
+          <div className='font-bold text-xl my-1 md:my-2'>
+            {productData.title}
+          </div>
+          <div className='md:text-lg md:my-2'>₩{productData.price}</div>
+          <div className='font-serif text-sm md:my-2 md:font-thin'>
             {productData.choice ? 'Conscious choice' : ''}
           </div>
           <div className='my-2'>
             <div>사이즈</div>
-            {/* <select>
-              <optgroup label='Size'>
-                {productData.size.map((sizeOption) => (
-                  <option>{sizeOption}</option>
-                ))}
-              </optgroup>
-            </select> */}
-            <ul className='flex'>
+            <ul className='flex mt-1 md:justify-center'>
               {productData.size.map((sizeOption, key) => (
                 <li key={key}>
-                  <button className='border-2 border-stone-800 rounded-md px-12 py-4 mx-2 my-2 cursor-pointer focus:border-red-500'>
+                  <button className='border-2 border-stone-800 rounded-md px-4 md:px-12 py-2 md:py-4 mr-4 cursor-pointer focus:border-red-500'>
                     {sizeOption}
                   </button>
                 </li>
               ))}
             </ul>
           </div>
-          <div className='my-2'>
-            <button className='bg-stone-800 text-stone-100 rounded p-2 w-96 hover:bg-stone-600'>
+          <div className='mt-24 md:w-96 md:mt-4'>
+            <button className='bg-stone-800 text-stone-100 rounded my-6 p-2 w-full hover:bg-stone-600'>
               추가
             </button>
           </div>
@@ -81,5 +108,10 @@ export default function Product() {
     );
   }
 
-  return <main className='flex justify-center my-12'>{content}</main>;
+  // return <main className='flex justify-center my-12'>{content}</main>;
+  return (
+    <main className='p-6 grid grid-cols-1 md:grid-cols-2 md:my-6'>
+      {content}
+    </main>
+  );
 }
