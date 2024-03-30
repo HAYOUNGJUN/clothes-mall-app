b6360f24f461ba51fa038de063c29b07060f6f71
Youngjun Ha <dudwnsk011@gmail.com>
2024-03-30 18:59:39 +0900

    delete unnecessary annotation

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -41,38 +41,6 @@ export default function Product() {
   }
 
   if (productData) {
-    // content = (
-    //   <>
-    //     <div className='w-96'>
-    //       <img src={productData.img} alt={productData.title} />
-    //     </div>
-    //     <div className='ml-8'>
-    //       <div className='text-xl my-2 font-bold'>{productData.title}</div>
-    //       <div className='text-lg my-2'>₩{productData.price}</div>
-    //       <div className='text-sm my-2 font-thin'>
-    //         {productData.choice ? 'Conscious choice' : ''}
-    //       </div>
-    //       <div className='my-2'>
-    //         <div>사이즈</div>
-    //
-    //         <ul className='flex'>
-    //           {productData.size.map((sizeOption, key) => (
-    //             <li key={key}>
-    //               <button className='border-2 border-stone-800 rounded-md px-12 py-4 mx-2 my-2 cursor-pointer focus:border-red-500'>
-    //                 {sizeOption}
-    //               </button>
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       </div>
-    //       <div className='my-2'>
-    //         <button className='bg-stone-800 text-stone-100 rounded p-2 w-96 hover:bg-stone-600'>
-    //           추가
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </>
-    // );
     content = (
       <>
         <div className='my-2 justify-self-center md:w-96 md:justify-self-end'>
@@ -108,7 +76,6 @@ export default function Product() {
     );
   }
 
-  // return <main className='flex justify-center my-12'>{content}</main>;
   return (
     <main className='p-6 grid grid-cols-1 md:grid-cols-2 md:my-6'>
       {content}
